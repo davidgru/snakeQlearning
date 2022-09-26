cd02a7afbd2da05f9dfa9f37a0230c3cb25269dd
David Grün <david.gruen.git@gmail.com>
2022-09-26 16:31:03 +0200

    add pygame display

---
+++ display.py
@@ -0,0 +1,41 @@
+import pygame
+
+from snakeMDP import BODY_ID, HEAD_ID, FOOD_ID, EMPTY_ID
+
+
+class Display:
+
+    def __init__(self, height, width, tile_size_px):
+        self.height = height
+        self.width = width
+        self.tile_size = tile_size_px
+
+        surface_height = height * tile_size_px
+        surface_width = width * tile_size_px
+        
+        self.surface = pygame.display.set_mode((surface_width, surface_height))
+
+
+    def draw(self, world):
+        self.surface.fill((0, 0, 0)) # black background
+        for y, row in enumerate(world):
+            for x, id_ in enumerate(row):
+                if id_ == BODY_ID:
+                    color = (255, 255, 255) # white
+                elif id_ == HEAD_ID:
+                    color = (255, 0, 0) # red
+                elif id_ == FOOD_ID:
+                    color = (0, 255, 0) # green
+                else:
+                    continue # dont draw empty tiles
+
+                rect = pygame.Rect(x * self.tile_size, y * self.tile_size, self.tile_size, self.tile_size)
+                pygame.draw.rect(self.surface, color, rect)
+        pygame.display.flip()
+
+
+    def update(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit(0)
+
--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from snakeMDP import Action, SnakeMDP
+from display import Display
 
-snake = SnakeMDP(10, 10, 20.0, -100.0, 0.1)
+from time import sleep
+
+HEIGHT = 10
+WIDTH = 10
+TILE_SIZE = 20
+
+snake = SnakeMDP(HEIGHT, WIDTH, 20.0, -100.0, 0.1)
+display = Display(HEIGHT, WIDTH, TILE_SIZE)
+
 
 state = snake.sample_start_state()
 
-print(state.world)
-
-while state:
-    print(f'{snake.reward(state, Action.LEFT)}')
+while True:
+    display.draw(state.world)
     state = snake.next(state, Action.LEFT)
-    if state:
-        print(state.world)
+
+    if not state:
+        state = snake.sample_start_state()
+
+    display.update()
+    sleep(0.5)
+    
