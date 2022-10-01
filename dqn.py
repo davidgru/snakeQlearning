f3ca4e5258554665af4b32a8872d84566482013f
David Grün <david.gruen.git@gmail.com>
2022-10-01 23:48:16 +0200

    change to CNN

--- dqn.py
+++ dqn.py
@@ -2,35 +2,10 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class DQN(nn.Module):
+class CNN(nn.Module):
 
     def __init__(self, height, width, device):
-        super(DQN, self).__init__()
-        
-        self.height = height
-        self.width = width
-        self.device = device
-
-        if self.height != 10 or self.width != 10:
-            raise "Height and width are fixed for now!"
-
-        self.conv1 = nn.Conv2d(1, 8, kernel_size=3, stride=1)
-        self.conv2 = nn.Conv2d(8, 16, kernel_size=3, stride=1)
-        self.conv3 = nn.Conv2d(16, 32, kernel_size=3, stride=1)
-        self.dense1 = nn.Linear(4*4*32, 4) # Output 4 Actions (left, right, up, down)
-
-    def forward(self, x):
-        x = x.to(self.device)
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
-        return self.dense1(x.view(x.size(0), -1))
-
-
-class ResNet(nn.Module):
-
-    def __init__(self, height, width, device):
-        super(ResNet, self).__init__()
+        super(CNN, self).__init__()
         
         self.height = height
         self.width = width
@@ -47,48 +22,19 @@ class ResNet(nn.Module):
         self.conv31 = nn.Conv2d(16, 16, kernel_size=3, stride=1, padding=1)
         self.conv32 = nn.Conv2d(16, 16, kernel_size=3, stride=1, padding=1)
 
-        self.dense1 = nn.Linear(10*10*16, 4) # Output 4 Actions (left, right, up, down)
+        self.dense1 = nn.Linear(10*10*16, 4)
+
 
     def forward(self, x):
         x = x.to(self.device)
 
         x = F.relu(self.conv1(x))
 
-        y = F.relu(self.conv21(x))
-        y = F.relu(self.conv22(y))
-        x = x + y
+        x = F.relu(self.conv21(x))
+        x = F.relu(self.conv22(x))
 
-        y = F.relu(self.conv31(x))
-        y = F.relu(self.conv32(y))
-        x = x + y
-
+        x = F.relu(self.conv31(x))
+        x = F.relu(self.conv32(x))
+        
         return self.dense1(x.view(x.size(0), -1))
 
-
-class CNN(nn.Module):
-
-    def __init__(self, height, width, device):
-        super(CNN, self).__init__()
-        
-        self.height = height
-        self.width = width
-        self.device = device
-
-        if self.height != 10 or self.width != 10:
-            raise "Height and width are fixed for now!"
-
-        self.conv1 = nn.Conv2d(1, 16, kernel_size=5, stride=1, padding=2)
-        self.conv2 = nn.Conv2d(16, 16, kernel_size=5, stride=1, padding=2)
-        self.conv3 = nn.Conv2d(16, 16, kernel_size=5, stride=1, padding=2)
-        self.dense1 = nn.Linear(10*10*16, 256)
-        self.dense2 = nn.Linear(256, 4)
-
-
-    def forward(self, x):
-        x = x.to(self.device)
-
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
-        x = F.relu(self.dense1(x.view(x.size(0), -1)))
-        return self.dense2(x)
